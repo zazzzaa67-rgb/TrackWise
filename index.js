98bc6ff5af23cf56b32c735c5fa0e983c36a27d5
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-05-21 16:54:19 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// remember to use data , includes , filter .
-// use one EventListener to listen to all page with e.target.____
-// use logical NOT operator (!)
-// use toggle
-// practice map(), forEach() , for of, normal for loop ,continue and break, and join() method do not start write javaScript code if did not do that
-//⚠️  do not use innerHTML with inputs or any data
-//use uuid 
-// use arrow funcation
+
+document.addEventListener("click" , function(e){
+    console.log(e.target)
+    if(e.target.id === "symbol"){
+        document.querySelector("nav").classList.toggle("hide")
+
+        
+    }
+
+})
