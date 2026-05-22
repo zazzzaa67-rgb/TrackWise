c5589323e6b64bf1a6e0a1693afc320dd9e9d196
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-05-22 18:10:27 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 // practice destructuring objects
 document.addEventListener("click" , function(e){
-    console.log(e.target)
     if(e.target.id === "symbol"){
-        document.querySelector("nav").classList.toggle("hide")
-
-        
+        document.querySelector("nav").classList.toggle("show")     
     }
+    const pages = document.getElementsByClassName("page")
 
+    //************ Navigation ***********/ 
+    if (e.target.id == "Addbtn"){
+        
+        render(document.getElementById("add"))
+    }else if(e.target.id == "reportsbtn"){
+        render(document.getElementById("reports"))
+    }else if(e.target.id == "Historybtn"){
+        render(document.getElementById("history"))
+    }else if(e.target.id == "settingsbtn"){
+        render(document.getElementById("s"))
+    }
+    
+    function render(item){
+        for(let page of pages ){
+            page.classList.remove("show")
+            page.classList.add("hide") 
+        }
+        const show = document.getElementById(item)
+        item.classList.remove("hide")
+        item.classList.add("show")
+    }
+    
 })
