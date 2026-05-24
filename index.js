72dcfdd813042d54818611bf675d3195327c2687
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-05-24 14:36:34 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 // practice destructuring objects
-document.addEventListener("click" , function(e){
-    function showNav(){
-        if(e.target.id === "symbol"){
-            document.querySelector("nav").classList.toggle("show")     
-        }
-}
-    showNav()
-    const pages = document.getElementsByClassName("page")
 
+document.addEventListener("click" , function(e){
+    const pages = document.getElementsByClassName("page")
+    const historyPage = document.getElementById("history")
+    const transactions = document.getElementById("transactions")
+    const usertransactions = []
     //************ Navigation ***********/ 
     if (e.target.id == "Addbtn"){
         
         render(document.getElementById("add"))
-        showNav()
+        hideNav()
     }else if(e.target.id == "reportsbtn"){
         render(document.getElementById("reports"))
-        showNav()
+        hideNav()
     }else if(e.target.id == "Historybtn"){
         render(document.getElementById("history"))
-        showNav()
+        hideNav()
     }else if(e.target.id == "settingsbtn"){
         render(document.getElementById("settings"))
-        showNav()
+        hideNav()
     }else if(e.target.id == "Homebtn") {
         render(document.getElementById("home-sec"))
-        showNav()
+        hideNav()
     }
-    
+    if(e.target.id == "add-info" ){
+        addform.addEventListener("submit" , e => {
+            e.preventDefault()
+            const data = new FormData(addform)
+            const addform = document.getElementById("add-info")
+            const usaddinfo = document.getElementsByClassName("adding-info")
+            usertransactions.push(
+                `<div>
+
+                </div>
+                `
+            // We will Start from here After 2 Hours man **********
+            )
+            
+            
+        } )
+
+
+    }
+    function showNav(){
+        if(e.target.id === "symbol"){
+            document.querySelector("nav").classList.toggle("show")     
+        }
+    }
+    showNav()
+    function hideNav(){
+        document.querySelector("nav").classList.remove("show")
+    }
     function render(item){
         for(let page of pages ){
             page.classList.remove("show")
@@ -35,5 +59,5 @@ document.addEventListener("click" , function(e){
         const show = document.getElementById(item)
         item.classList.remove("hide")
         item.classList.add("show")
-    }    
+    }
 })
